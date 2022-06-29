ebcc1ce2f19b981335954d7f46d917038cbb0ef2
Akbar <akbarbadmus07@gmail.com>
2022-06-29 16:36:21 +0100

    node call back and errror handling

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var rect = require('./rectangle')
+const rect = require('./rectangle')
 
     function solveRect(l,b) {
         console.log("solving for rectangle with l =" + l +"and b ="+ b)
-        if(l<=0 ||b<=0){
-            console.log("rect dimention should e greater than zero: l =")
-        }
-        else{
-            console.log("area of rect is " + rect.area(l,b))
-            console.log("perimeter of rect is" + rect.perimeter(l,b))
-        }
+       
+        rect(l,b, (err, rectangle) => {
+            if(err){
+                console.log("ERROR: ", err.message)
+            }
+            else{
+                console.log("The area of the rect of dimentions l=" + l + " and b " + b + " is " + rectangle.area() )
+                console.log("The perimeter of the rect of dimentions l=" + l + " and b " + b + " is " + rectangle.perimeter() )
+            }
+
+        })
+        console.log("this statment is after the call to rect")
 
     }
 
